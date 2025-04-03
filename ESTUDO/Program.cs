49582797c823d2a264a7485dcc79c3b03ffd23a3
ERROR_404 <ERROR_404@DESKTOP-IJHTB17>
2025-04-03 14:41:40 -0300

    Adicionar arquivos de projeto.

---
+++ ESTUDO/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string Nome;
+        int N1, N2, N3, N4, RA;
+
+        Console.WriteLine("Informe o nome do aluno");
+        Nome = Console.ReadLine();
+
+        Console.WriteLine("Informe o RA do aluno");
+        RA = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe a 1 nota");
+        N1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe a 2 nota");
+        N2 = int.Parse(Console.ReadLine());
+            
+        Console.WriteLine("Informe a 3 nota");
+        N3 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe a 4 nota");
+        N4 = int.Parse(Console.ReadLine());
+
+        double Media = (N1 + N2 + N3 + N4)/4;
+
+        Console.WriteLine("A media final do aluno: " + Nome + " é: " + Media);
+    }
+}
