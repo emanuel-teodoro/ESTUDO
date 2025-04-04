4b7da34bb72637d1384ee3df079c587677af7c66
emanuel-teodoro <168236917+emanuel-teodoro@users.noreply.github.com>
2025-04-04 11:59:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //CODIGO COMPARAÇÃO ENTRE VALORES USANDO BOOL
+        //int N1, N2;
+
+        //Console.WriteLine("Informe valor N1");
+        //N1 = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Informe valor N2");
+        //N2 = int.Parse(Console.ReadLine());
+
+        //bool resultado = (N1 == N2);
+
+        //Console.WriteLine(resultado);
+
+        //CODIGO UTILIZANDO OPERADORES LOGICOS
+        //int N1, N2, N3, N4;
+
+        //Console.WriteLine("Informe as notas do aluno");
+        //N1 = int.Parse(Console.ReadLine());
+        //N2 = int.Parse(Console.ReadLine());
+        //N3 = int.Parse(Console.ReadLine());
+        //N4 = int.Parse(Console.ReadLine());
+
+        //int media = (N1 + N2 + N3 + N4) / 4;
+
+        //Console.WriteLine("A LUNO EM RECUPERAÇÃO? ");
+        //Console.WriteLine((media > 3) && (media < 6));
+    }
+}
